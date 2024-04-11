7889d14ead1c7a12dc748eae93ad901753e87f6a
Mandi <mandi.gansauge@gmail.com>
2024-04-11 23:35:37 +0200

    specialty component js

--- src/components/aboutComponents/Specialty.js
+++ src/components/aboutComponents/Specialty.js
@@ -1,5 +1,19 @@
-function Specialty() {
+function Specialty(props) {
+    const projectTypesList = props.projectTypes.map(projectType => <li>{projectType}</li>)
     return(
+        <div className="specialty">
+            <div className="specialty-title">
+                {props.iconSrc && <img src={props.iconSrc} alt="" />}
+                <h2>{props.title}</h2>
+            </div>
+            <div>
+                <p>Types of projects</p>
+                <ul>
+                    {projectTypesList}
+                </ul>
+            </div>
+                  
+        </div>
     )
     
 }
